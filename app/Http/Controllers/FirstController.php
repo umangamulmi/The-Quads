e76fd84284aab0164923801fb00514846f067578
Amit Pathak <amitpathak@my.unt.edu>
2019-11-14 16:46:14 -0600

    item adding controller and pin code validation changes for cash register function

--- app/Http/Controllers/FirstController.php
+++ app/Http/Controllers/FirstController.php
@@ -4,7 +4,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Actor;
-use Session ;
+use Session;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -14,34 +14,32 @@ class FirstController extends Controller
     {
 //        var_dump($request);
         $result = Actor::where('pincode', sha1($request->get('pincode')))->first();
-        if($result !== null){
-            if($result->role === 'cashier' && $request->get('title') === 'Administrator')
-                return response()->json(['success'=>'Permission Error']);
-            if($result->role === 'manager' && ($request->get('title') === 'Cash')){
-                return response()->json(['success'=>'Permission Error']);
+        if ($result !== null) {
+            if ($result->role === 'cashier' && $request->get('title') === 'Administrator')
+                return response()->json(['success' => 'Permission Error']);
+            if ($result->role === 'manager' && ($request->get('title') === 'Cash')) {
+                return response()->json(['success' => 'Permission Error']);
             }
-            $redirectUrl ='/clock';
-            switch ($request->get('title')){
+            $redirectUrl = '/clock';
+            switch ($request->get('title')) {
                 case 'Cash':
                     $redirectUrl = '/cash';
                     break;
                 case 'Administrator':
-                    $redirectUrl = '/' . $result->role;
+                    $redirectUrl = '/admin';
             }
-//            if($request->session()->has('user_name')){
-//
-//            }
             $session_id = Session::getId();
-            $request->session()->put("user_name",$result->name);
-            $request->session()->put("user_id",$result->id);
+            $request->session()->put("user_name", $result->name);
+            $request->session()->put("user_role", $result->role);
+            $request->session()->put("user_id", $result->id);
             DB::table('session')->insert(
-                ['session' => $session_id,'uid' => $result->id]
+                ['session' => $session_id, 'uid' => $result->id]
             );
             return response()->json([
-                'success'=>'ok',
-                'url'=>$redirectUrl]);
-        }else{
-            return response()->json(['success'=>'Pincode Error']);
+                'success' => 'ok',
+                'url' => $redirectUrl]);
+        } else {
+            return response()->json(['success' => 'Pincode Error']);
         }
     }
 }
