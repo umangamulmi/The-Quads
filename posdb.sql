76cc8a057aef537b2c9694e7de76af4b644f420a
Samikshya <samikshyaluitel@dynamic-10-125-202-32.dynamic.unt.edu>
2019-11-14 17:43:22 -0600

    sql database update,sale and report table

--- posdb.sql
+++ posdb.sql
@@ -12,7 +12,7 @@ MySQL - 10.4.8-MariaDB : Database - posdb
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`posdb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`posdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
 USE `posdb`;
 
@@ -28,14 +28,14 @@ CREATE TABLE `actors` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `actors` */
 
 insert  into `actors`(`id`,`name`,`pincode`,`role`,`created_at`,`updated_at`) values 
-(11,'Administrator','7c4a8d09ca3762af61e59520943dc26494f8941b','admin','2019-11-01 14:15:18','2019-11-01 14:15:18'),
-(12,'Manager','908f704ccaadfd86a74407d234c7bde30f2744fe','manager','2019-11-01 14:15:18','2019-11-01 14:15:18'),
-(13,'Cashier','118a43489e2f9ab66823eabdada672c906bb387f','cashier','2019-11-01 14:15:18','2019-11-01 14:15:18');
+(1,'Administrator','7c4a8d09ca3762af61e59520943dc26494f8941b','admin','2019-11-04 23:35:15','2019-11-04 23:35:15'),
+(2,'Manager','908f704ccaadfd86a74407d234c7bde30f2744fe','manager','2019-11-04 23:35:16','2019-11-04 23:35:16'),
+(3,'Cashier','118a43489e2f9ab66823eabdada672c906bb387f','cashier','2019-11-04 23:35:16','2019-11-04 23:35:16');
 
 /*Table structure for table `contacts` */
 
@@ -59,23 +59,24 @@ CREATE TABLE `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `migrations` */
 
 insert  into `migrations`(`id`,`migration`,`batch`) values 
 (1,'2019_10_28_000000_create_actors_table',1),
-(2,'2019_10_31_081825_create_contacts_table',2),
-(3,'2019_10_31_083223_create_contacts_table',3),
-(5,'2019_11_01_140853_create_products_table',4),
-(8,'2019_11_02_131104_create_work_hours_table',5);
+(2,'2019_10_31_083223_create_contacts_table',1),
+(4,'2019_11_02_131104_create_work_hours_table',2),
+(8,'2019_11_01_140853_create_products_table',3),
+(18,'2019_11_11_074157_create_tranactions_table',7),
+(20,'2019_11_11_076105_create_sales_table',8);
 
 /*Table structure for table `products` */
 
 DROP TABLE IF EXISTS `products`;
 
 CREATE TABLE `products` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `description` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `price` decimal(15,2) DEFAULT NULL,
@@ -83,14 +84,22 @@ CREATE TABLE `products` (
   `updated_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `products` */
 
-insert  into `products`(`id`,`name`,`description`,`price`,`created_at`,`updated_at`,`deleted_at`) values 
-(1,'1','12',11.00,'2019-11-01 16:51:44','2019-11-01 16:59:08','2019-11-01 16:59:08'),
-(2,'11','fsdfsdfsdfsd',0.05,'2019-11-01 17:08:48','2019-11-01 18:44:11',NULL),
-(3,'hhhh','sdsd',0.02,'2019-11-02 14:10:43','2019-11-02 14:10:43',NULL);
+/*Table structure for table `sales` */
+
+DROP TABLE IF EXISTS `sales`;
+
+CREATE TABLE `sales` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `transaction_id` bigint(20) unsigned DEFAULT NULL,
+  `product_id` bigint(20) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `sales` */
 
 /*Table structure for table `session` */
 
@@ -101,45 +110,27 @@ CREATE TABLE `session` (
   `uid` int(10) DEFAULT NULL,
   `session` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=204 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=274 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 /*Data for the table `session` */
 
-insert  into `session`(`id`,`uid`,`session`) values 
-(170,11,'5cxklh4Q6sPAdTPVmqKMkQb67Qm4aExgqRV2plCP'),
-(171,11,'5cxklh4Q6sPAdTPVmqKMkQb67Qm4aExgqRV2plCP'),
-(172,11,'XSvxbKwNuAlYI6rpomctlalK1iVkJ2vFrV3VDNY5'),
-(173,11,'a2cK1ZXotomozm2xLgMvB3tOtr4AkiCaFpfOeWJR'),
-(174,11,'ktMt3W9cDcIvVFVEqU2QwSeqZCUyqzQ89IgjrR02'),
-(175,11,'mcsL7fpkHJXqdTRu8EKqppvoUt72fUNtqQhm5Eys'),
-(176,11,'mcsL7fpkHJXqdTRu8EKqppvoUt72fUNtqQhm5Eys'),
-(177,11,'mcsL7fpkHJXqdTRu8EKqppvoUt72fUNtqQhm5Eys'),
-(178,11,'mcsL7fpkHJXqdTRu8EKqppvoUt72fUNtqQhm5Eys'),
-(179,11,'CmXglosMFnKis0cyRCLWaozw8Ef7GjdNXJfaVrfA'),
-(180,11,'CmXglosMFnKis0cyRCLWaozw8Ef7GjdNXJfaVrfA'),
-(181,11,'CmXglosMFnKis0cyRCLWaozw8Ef7GjdNXJfaVrfA'),
-(182,11,'CmXglosMFnKis0cyRCLWaozw8Ef7GjdNXJfaVrfA'),
-(183,11,'CmXglosMFnKis0cyRCLWaozw8Ef7GjdNXJfaVrfA'),
-(184,11,'CmXglosMFnKis0cyRCLWaozw8Ef7GjdNXJfaVrfA'),
-(185,11,'CmXglosMFnKis0cyRCLWaozw8Ef7GjdNXJfaVrfA'),
-(186,11,'CmXglosMFnKis0cyRCLWaozw8Ef7GjdNXJfaVrfA'),
-(187,11,'CmXglosMFnKis0cyRCLWaozw8Ef7GjdNXJfaVrfA'),
-(188,11,'CmXglosMFnKis0cyRCLWaozw8Ef7GjdNXJfaVrfA'),
-(189,11,'73V6hOePUC4GmwGXzKz828QYFDpqrTcPgnR0od0N'),
-(190,11,'ZSQwzd0KnWTdPqEtlJTGQD8Xt2kSGCzWtnP2v0HW'),
-(191,11,'ZSQwzd0KnWTdPqEtlJTGQD8Xt2kSGCzWtnP2v0HW'),
-(192,11,'ZSQwzd0KnWTdPqEtlJTGQD8Xt2kSGCzWtnP2v0HW'),
-(193,11,'ZSQwzd0KnWTdPqEtlJTGQD8Xt2kSGCzWtnP2v0HW'),
-(194,11,'ZSQwzd0KnWTdPqEtlJTGQD8Xt2kSGCzWtnP2v0HW'),
-(195,11,'ZSQwzd0KnWTdPqEtlJTGQD8Xt2kSGCzWtnP2v0HW'),
-(196,11,'ZSQwzd0KnWTdPqEtlJTGQD8Xt2kSGCzWtnP2v0HW'),
-(197,11,'ZSQwzd0KnWTdPqEtlJTGQD8Xt2kSGCzWtnP2v0HW'),
-(198,11,'ZSQwzd0KnWTdPqEtlJTGQD8Xt2kSGCzWtnP2v0HW'),
-(199,11,'ZSQwzd0KnWTdPqEtlJTGQD8Xt2kSGCzWtnP2v0HW'),
-(200,11,'ZSQwzd0KnWTdPqEtlJTGQD8Xt2kSGCzWtnP2v0HW'),
-(201,11,'ZSQwzd0KnWTdPqEtlJTGQD8Xt2kSGCzWtnP2v0HW'),
-(202,11,'ZSQwzd0KnWTdPqEtlJTGQD8Xt2kSGCzWtnP2v0HW'),
-(203,11,'ZSQwzd0KnWTdPqEtlJTGQD8Xt2kSGCzWtnP2v0HW');
+/*Table structure for table `transactions` */
+
+DROP TABLE IF EXISTS `transactions`;
+
+CREATE TABLE `transactions` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `actor_id` bigint(20) unsigned DEFAULT NULL,
+  `customer_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total` double(8,2) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tranactions_actor_id_foreign` (`actor_id`),
+  CONSTRAINT `tranactions_actor_id_foreign` FOREIGN KEY (`actor_id`) REFERENCES `actors` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `transactions` */
 
 /*Table structure for table `work_hours` */
 
@@ -147,18 +138,16 @@ DROP TABLE IF EXISTS `work_hours`;
 
 CREATE TABLE `work_hours` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `actor_id` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `actor_id` bigint(20) unsigned DEFAULT NULL,
   `login_status` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  PRIMARY KEY (`id`),
+  KEY `work_hours_actor_id_foreign` (`actor_id`),
+  CONSTRAINT `work_hours_actor_id_foreign` FOREIGN KEY (`actor_id`) REFERENCES `actors` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `work_hours` */
-
-insert  into `work_hours`(`id`,`actor_id`,`login_status`,`created_at`,`updated_at`) values 
-(47,'11','out','2019-11-03 10:13:47','2019-11-03 10:14:42'),
-(48,'11','in','2019-11-03 11:00:47','2019-11-03 11:00:47');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
